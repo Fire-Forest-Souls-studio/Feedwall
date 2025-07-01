22358810149fc1fb40834c8a77596c3cfe5d2a46
matve <matkonov2009@gmail.com>
2025-07-01 18:02:22 +0300

    alpha 0.1 - Частично реализовано главное меню приложения FeedWall - Добавлена кнопка "Включить" с возможностью переключения состояния - Добавлена иконка "Настройки" - Добавлен подзаголовок с описанием функционала - Интерфейс пока не полностью завершён, продолжу дорабатывать

--- composeApp/src/commonMain/kotlin/fireforestsouls/feedwall/App.kt
+++ composeApp/src/commonMain/kotlin/fireforestsouls/feedwall/App.kt
@@ -14,29 +14,118 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Icon
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.background
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.*
 
-import feedwall.composeapp.generated.resources.Res
-import feedwall.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFF0A0A0A)
+    ) {
+        var isChecked by remember { mutableStateOf(false) }
+        Column(modifier = Modifier.fillMaxSize()) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(46.dp)
+                        .offset(x = 8.dp, y = 24.dp)
+                        .background(
+                            color = Color(0xFF141414),
+                            shape = RoundedCornerShape(18.dp)
+                        ),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "⚙️", fontSize = 28.sp, color = Color(0xFF888888))
+                }
+
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .offset(x = 8.dp),
+                horizontalArrangement = Arrangement.Start
+            ) { Text(text = "FeedWall", fontSize = 42.sp, color = Color(0xFFE8E8E8)) }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .offset(x = 8.dp, y = -24.dp),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Text(
+                    text = "Changes lock screen based on Telegram channels posts",
+                    fontSize = 16.sp,
+                    color = Color(0xFFACACAC)
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .offset(x = 8.dp, y = -24.dp),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Box(
+                    modifier = Modifier
+                        .width(300.dp)
+                        .height(52.dp)
+                        .background(
+                            color = Color(0xFF141414),
+                            shape = RoundedCornerShape(14.dp)
+                        ),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Enable",
+                            fontSize = 18.sp,
+                            color = Color(0xFFD2D2D2),
+                        )
+
+                        Spacer(modifier = Modifier.weight(1f))
+
+                        Switch(
+                            checked = isChecked,
+                            onCheckedChange = { isChecked = it },
+                            colors = SwitchDefaults.colors(
+                                checkedThumbColor = Color(color = 0xFF5C80B8),
+                                uncheckedThumbColor = Color(color = 0xFFe1e4eb),
+                                checkedTrackColor = Color(0xFF292B31),
+                                uncheckedTrackColor = Color(0xFF666666)
+                            )
+                        )
+                    }
                 }
             }
         }
--- composeApp/src/desktopMain/kotlin/fireforestsouls/feedwall/main.kt
+++ composeApp/src/desktopMain/kotlin/fireforestsouls/feedwall/main.kt
@@ -3,10 +3,18 @@ package fireforestsouls.feedwall
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.unit.DpSize
+
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Feedwall",
+        state = WindowState(
+            size = DpSize(width = 360.dp, height = 800.dp)
+        )
+
     ) {
         App()
     }
